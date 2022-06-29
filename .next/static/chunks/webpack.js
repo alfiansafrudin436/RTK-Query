380a299a51068b86fc57bb6035901d7ff7bdce12
alfiansafrudin436 <alfiansafrudin436@gmail.com>
2022-06-29 09:29:43 +0700

    git ignone

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8d81cd7e164c38f2"; }
+/******/ 		__webpack_require__.h = function() { return "2b826a69e27589db"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
